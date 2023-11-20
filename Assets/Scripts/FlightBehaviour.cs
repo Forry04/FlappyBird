9f44f2da8d4feddac3025a931bea1fb943e42d68
Erdős Barnabás <ebarni04@gmail.com>
2023-11-20 11:51:54 +0100

    🐱‍👤

---
+++ Assets/Scripts/FlightBehaviour.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FlightBehaviour : MonoBehaviour
+{
+    [SerializeField] private float _velocity = 1.5f;
+    [SerializeField] private float _rotationSpeed = 10f;
+    private Rigidbody2D _rigidbody2D;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            _rigidbody2D.velocity = Vector2.up * _velocity;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, _rigidbody2D.velocity.y * _rotationSpeed);
+    }
+}
